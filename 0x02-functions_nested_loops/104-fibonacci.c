0b6d3650dbe01c3dd5bf3039a2f32e11f55310ca
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-04-26 07:51:05 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,13 +34,13 @@ int main(void)
 				boolean2 = 0;
 			}
 			fn = (n11 = n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
+			fn = n1 + n2 + (fn / 1000000000);
 			printf(", %ld", fn);
-			printf("%ld", fn2 % 100000000);
+			printf("%ld", fn % 100000000);
 			n1 = n2;
 			n11 = n22;
 			n2 = fn;
-			n22 = (fn2 % 1000000000);
+			n22 = (fn % 1000000000);
 		}
 		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
