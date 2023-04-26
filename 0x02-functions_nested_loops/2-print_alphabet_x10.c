f8e09ecbdf9da1a7901b0f39ecd5d17ec3e453b2
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-04-26 05:14:49 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a - z ten times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
