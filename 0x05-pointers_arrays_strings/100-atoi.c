a426ef02f3061d27f70a4ab51b4fa1bb98953b79
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-05-02 07:27:42 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,28 +5,29 @@
  * @s: input string to be converted
  * Return: integer.
  */
+
 int _atoi(char *s)
 {
-	unsigned int length = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (*(s + length) != '\0')
+	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + length) < '0' || *(s + length) > '9'))
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 
-		if (*(s + length) == '-')
+		if (*(s + count) == '-')
 			pn *= -1;
 
-		if ((*(s + length) >= '0') && (*(s + length) <= '9'))
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
 			size++;
 		}
-		length++;
+		count++;
 	}
 
-	for (i = length - size; i < length; i++)
+	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
