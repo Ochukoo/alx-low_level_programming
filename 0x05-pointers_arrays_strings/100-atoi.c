7987f4b020a8cc18fdff0115bdd91cba549f501a
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-05-02 07:20:55 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,26 @@
  */
 int _atoi(char *s)
 {
-	unsigned int len = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	unsigned int length = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (*(s + len) != '\0')
+	while (*(s + length) != '\0')
 	{
-		if (size > 0 && (*(s + len) < '0' || *(s + len) > '9'))
+		if (size > 0 && (*(s + length) < '0' || *(s + length) > '9'))
 			break;
 
-		if (*(s + len) == '-')
+		if (*(s + length) == '-')
 			pn *= -1;
 
-		if ((*(s + len) >= '0') && (*(s + len) <= '9'))
+		if ((*(s + length) >= '0') && (*(s + length) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
 			size++;
 		}
-		len++;
+		length++;
 	}
 
-	for (i = len - size; i < len; i++)
+	for (i = length - size; i < length; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
