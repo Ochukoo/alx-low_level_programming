10eb66f98dcb1996740d4b9af6a88d21a5002dda
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-05-02 04:38:07 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include main.h
+
+/**
+ * reset_to_98 - updates the value of an integer to 98
+ * @i: pointer to the integer to update
+ *
+ * Return: void
+ */
+void reset_to_98(int *i)
+{
+	*i = 98;
+}
+
