74e3b95de960356a1ff40db8c5187feb28378a46
Ochuko Osawota <ochuko.osawota@gmail.com>
2023-05-02 06:08:48 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
